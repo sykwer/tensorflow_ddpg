86af719aa6a552aba0e66e45bc6b13447e7f2364
sykwer <sykwer@gmail.com>
2018-03-23 08:19:29 +0900

    Implement noise

--- ou_noise.py
+++ ou_noise.py
@@ -1,14 +1,29 @@
 import numpy as np
 
 class OUNoise:
-    def __init__(self, num_actions):
+    def __init__(self, num_actions, mu=0, theta=0.15, sigma=0.3):
         self.num_actions = num_actions
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+
+        self.reset()
 
     def generate(self):
-        # FIXME: pass
-        return np.random.random(self.num_actions)
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
 
     def reset(self):
-        # FIXME: pass
-        pass
+        self.state = np.ones(self.num_actions) * self.mu
+
+if __name__ == "__main__":
+    noise = OUNoise(3)
+    outputs = []
+    for _ in range(1000):
+        outputs.append(noise.generate())
+    import matplotlib.pyplot as plt
+    plt.plot(outputs)
+    plt.show()
 
